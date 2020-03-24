b86f5ba38d95c5194d3e5e579f0482fd21928b90
Moisés Carvalho <moisescarv27@hotmail.com>
2020-03-24 10:33:35 -0300

    Send message via JSON

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => response.send('Hello World'));
+app.get('/', (request, response) => {
+  response.json({
+    evento: 'Semana OmniStack 11.0',
+    aluno: 'Moisés Carvalho'
+  });
+});
 
 app.listen(3000);
